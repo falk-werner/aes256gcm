77ef2f38c5b96b6045b8956a3823506b60ba0358
Falk Werner <falk.werner@gmx.net>
2026-05-21 21:15:22 +0200

    Use secure default configuration based on OWASP recommendations
    See https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html for details,
    
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- lib/aes256gcm/constants.hpp
+++ lib/aes256gcm/constants.hpp
@@ -6,11 +6,11 @@
 namespace aes256gcm
 {
 
-constexpr size_t const kdf_salt_size = 8;
+constexpr size_t const kdf_salt_size = 16;
 constexpr size_t const nonce_size = 12;
 constexpr size_t const key_size = 32;
 constexpr size_t const tag_size = 16;
-constexpr unsigned int const kdf_iterations = 2048;
+constexpr unsigned int const kdf_iterations = 600 * 1000;
 constexpr char const kdf_digest[] = "sha256";
 constexpr char const pbkdf2_algorithm[] = "PBKDF2";
 constexpr char const encryption_method[] = "AES256-GCM";
