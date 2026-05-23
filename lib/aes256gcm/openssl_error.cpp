eec140d77b9ae51ed96070bff6e1705162229477
Falk Werner <falk.werner@gmx.net>
2026-05-23 14:41:24 +0200

    provide complete openssl error stack
    Signed-off-by: Falk Werner <falk.werner@gmx.net>

--- lib/aes256gcm/openssl_error.cpp
+++ lib/aes256gcm/openssl_error.cpp
@@ -1,18 +1,35 @@
 #include "aes256gcm/openssl_error.hpp"
 #include <openssl/err.h>
+#include <sstream>
 
 namespace aes256gcm
 {
 
 openssl_error::openssl_error()
 {
-    m_error_code = ERR_get_error();
-    
+    std::stringstream message;
     constexpr size_t const buffer_size = 256;
     char buffer[buffer_size];
-    ERR_error_string_n(m_error_code, buffer, buffer_size);
 
-    m_error_message = buffer;
+    m_error_code = ERR_get_error();
+    ERR_error_string_n(m_error_code, buffer, buffer_size);
+    message << buffer;
+
+    auto error_code = ERR_get_error();
+    if (error_code != 0) {
+        message << '(';
+        while (error_code != 0) {
+            ERR_error_string_n(error_code, buffer, buffer_size);
+            message << ' ' << buffer;
+
+            error_code = ERR_get_error();
+        }
+        message << ')';
+    }
+    
+    
+
+    m_error_message = message.str();
 }
 
 char const * openssl_error::what() const noexcept
